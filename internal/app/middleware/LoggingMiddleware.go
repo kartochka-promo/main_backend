343f4bbe3fbad75417c27ab1d5373016c5a7f842
dmitry <boldin.dmitry@gmail.com>
2020-06-28 21:19:25 +0300

    monitoring fixed in LoggingMiddleware.go

--- internal/app/middleware/LoggingMiddleware.go
+++ internal/app/middleware/LoggingMiddleware.go
@@ -2,6 +2,8 @@ package middleware
 
 import (
 	"2020_1_drop_table/internal/pkg/metrics"
+	"2020_1_drop_table/internal/pkg/responses"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
@@ -9,6 +11,29 @@ import (
 	"strconv"
 	"time"
 )
+
+type LoggingResponseWriter struct {
+	wrapped  http.ResponseWriter
+	response responses.HttpResponse
+}
+
+func NewLoggingResponseWriter(wrapped http.ResponseWriter) *LoggingResponseWriter {
+	return &LoggingResponseWriter{wrapped: wrapped}
+}
+
+func (lrw *LoggingResponseWriter) Header() http.Header {
+	return lrw.wrapped.Header()
+}
+
+func (lrw *LoggingResponseWriter) Write(content []byte) (int, error) {
+	// error ignored because there are cases, where response is OK and body is nil(or contains photo)
+	_ = json.Unmarshal(content, &lrw.response)
+	return lrw.wrapped.Write(content)
+}
+
+func (lrw *LoggingResponseWriter) WriteHeader(statusCode int) {
+	lrw.wrapped.WriteHeader(statusCode)
+}
 
 func NewLoggingMiddleware(metrics *metrics.PromMetrics) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
@@ -19,11 +44,15 @@ func NewLoggingMiddleware(metrics *metrics.PromMetrics) mux.MiddlewareFunc {
 			}
 
 			reqTime := time.Now()
-			next.ServeHTTP(w, r)
+			w2 := NewLoggingResponseWriter(w)
+			next.ServeHTTP(w2, r)
 			respTime := time.Since(reqTime)
 			if r.URL.Path != "/metrics" {
-				//If status is 500 => response will be in panic middleware
-				metrics.Hits.WithLabelValues(strconv.Itoa(http.StatusOK), r.URL.Path, r.Method).Inc()
+				if w2.response.Errors == nil || len(w2.response.Errors) == 0 {
+					metrics.Hits.WithLabelValues(strconv.Itoa(http.StatusOK), r.URL.Path, r.Method).Inc()
+				} else {
+					metrics.Hits.WithLabelValues(strconv.Itoa(w2.response.Errors[0].Code), r.URL.Path, r.Method).Inc()
+				}
 				metrics.Timings.WithLabelValues(
 					strconv.Itoa(http.StatusOK), r.URL.String(), r.Method).Observe(respTime.Seconds())
 			}
