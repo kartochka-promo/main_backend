4b1ec214fcbe38991704ea129169a93850ef32d4
dmitry <boldin.dmitry@gmail.com>
2020-07-16 16:58:59 +0300

    name is not required in staff model

--- internal/microservices/staff/models/staff.go
+++ internal/microservices/staff/models/staff.go
@@ -5,7 +5,7 @@ import "time"
 //easyjson:json
 type Staff struct {
 	StaffID  int       `json:"id"`
-	Name     string    `json:"name" validate:"required,min=4,max=100"`
+	Name     string    `json:"name" validate:"min=4,max=100"`
 	Email    string    `json:"email" validate:"required,email"`
 	Password string    `json:"password" validate:"required,min=8,max=100"`
 	EditedAt time.Time `json:"editedAt" validate:"required"`
